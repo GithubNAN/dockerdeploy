9cdc09df4a4e82e3e8a2b623accd77ce29b0359a
Jack ZHANG <johnsonzead@gmail.com>
2017-07-31 14:47:44 +1000

    CD test

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push willrstern/sample-node
+docker push djz721/sample-node
 
-ssh deploy@159.203.127.59 << EOF
-docker pull willrstern/sample-node:latest
+ssh deploy@128.199.153.21 << EOF
+docker pull djz721/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi willrstern/sample-node:current || true
-docker tag willrstern/sample-node:latest willrstern/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 willrstern/sample-node:current
+docker tag djz721/sample-node:latest djz721/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 djz721/sample-node:current
 EOF
