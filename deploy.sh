cf48b9143e3364a19607b2e37ca41c9c03341d89
Jack ZHANG <johnsonzead@gmail.com>
2017-07-31 15:22:58 +1000

    CD corrected test8

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push djz721/sample-node
+docker image push djz721/sample-node
 
 ssh deploy@128.199.153.21 << EOF
-docker pull djz721/sample-node:latest
-docker stop web || true
-docker rm web || true
-docker rmi willrstern/sample-node:current || true
-docker tag djz721/sample-node:latest djz721/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 djz721/sample-node:current
+docker image pull djz721/sample-node:latest
+docker container stop web || true
+docker container rm web || true
+docker image rm djz721/sample-node:current || true
+docker image tag djz721/sample-node:latest djz721/sample-node:current
+docker container run -d --net app --restart always --name web -p 3000:3000 djz721/sample-node:current
 EOF
